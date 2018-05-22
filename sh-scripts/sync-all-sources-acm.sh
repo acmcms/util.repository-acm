0fa0cc2978b8ed56fffa1d6386ed06ae6fe080f3
Alexander Kharichev <myx@myx.co.nz>
2018-05-22 09:46:11 +0300

    * new async

--- sh-scripts/sync-all-sources-acm.sh
+++ sh-scripts/sync-all-sources-acm.sh
@@ -10,9 +10,9 @@ fi
 . "$APP/source/myx/util.repository-myx/sh-lib/syncing.sh.include"
 
 
-async gitSync "acm/util.repository-acm/"    "git@github.com:acmcms/util.repository-acm.git"
+async -2 gitSync "acm/util.repository-acm/"    "git@github.com:acmcms/util.repository-acm.git"
 
-async gitSync "acm/acm.impl-acmbsd/"        "git@github.com:vlapan/acmbsd.git"
+async -2 gitSync "acm/acm.impl-acmbsd/"        "git@github.com:vlapan/acmbsd.git"
 
 INF="$APP/source/acm/util.repository-acm/data/repository/repository.inf"
 ( set -e ; echo "# copied from $INF at `date`" ; cat "$INF" ) > "$APP/source/acm/repository.inf"
