6302c3eb35c908a901c8f9a212c0e1f85a46a82f
Alexander Kharichev <myx@myx.co.nz>
2018-05-23 21:21:35 +0300

    + repository installer

--- sh-scripts/sync-all-sources-acm.sh
+++ sh-scripts/sync-all-sources-acm.sh
@@ -7,14 +7,16 @@ if [ -z "$APP" ] ; then
 	[ -d "$APP/source" ] || ( echo "expecting 'source' directory." >&2 && exit 1 )
 fi
 
+[ -f "$APP/source/myx/util.repository-myx/sh-lib/syncing.sh.include" ] /
+	|| (myx.common lib/gitCloneSync "$APP/source/myx/util.repository-myx" "git@github.com:myx/util.repository-myx.git")
+	
 . "$APP/source/myx/util.repository-myx/sh-lib/syncing.sh.include"
-
 
 async -2 gitSync "acm/util.repository-acm/"    "git@github.com:acmcms/util.repository-acm.git"
 
 async -2 gitSync "acm/acm.impl-acmbsd/"        "git@github.com:vlapan/acmbsd.git"
 
+wait
+
 INF="$APP/source/acm/util.repository-acm/data/repository/repository.inf"
 ( set -e ; echo "# copied from $INF at `date`" ; cat "$INF" ) > "$APP/source/acm/repository.inf"
-
-wait
