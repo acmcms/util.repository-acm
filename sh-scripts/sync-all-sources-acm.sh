f6a0c34b068db8beb91ed9dfd6bb657eb61596ac
Alexander Kharichev <myx@myx.co.nz>
2018-05-25 18:06:16 +0300

    ! GitCloseSync target path

--- sh-scripts/sync-all-sources-acm.sh
+++ sh-scripts/sync-all-sources-acm.sh
@@ -11,7 +11,7 @@ fi
 . "`myx.common which lib/gitCloneSync`"
 
 async(){
-	Async -2 GitCloneSync "$@"
+	Async -2 GitCloneSync "$MMDAPP/source/$@"
 }
 
 async "acm/util.repository-acm/"    "git@github.com:acmcms/util.repository-acm.git"
