3619cd8b20edf4919217fd4f961e7713b036190e
Alexander I. Kharitchev <myx@myx.co.nz>
2021-01-27 21:47:45 +0200

    * better build output logging

--- source-process/builders/2201-acm-domain.sh
+++ source-process/builders/2201-acm-domain.sh
@@ -1,19 +1,30 @@
-MDSC_SOURCE="${MDSC_SOURCE:-$MMDAPP/cache/sources}"
-
 MakeProjectAcmDomain(){
+	local MDSC_SOURCE="${MDSC_SOURCE:-$MMDAPP/cached/sources}"
+	local MDSC_OUTPUT="${MDSC_OUTPUT:-$MMDAPP/output/distro}"
+	
 	local projectName="$1"
 	if [ -z "$projectName" ] ; then
-		echo "MakeProjectAe3Packages: 'projectName' argument is required!" >&2 ; exit 1
+		echo "MakeProjectAcmDomain: 'projectName' argument is required!" >&2 ; exit 1
 	fi
 	
+	[ -z "$MDSC_DETAIL" ] || echo "MakeProjectAcmDomain: project: $projectName, source: $MDSC_SOURCE, output: $MDSC_OUTPUT" >&2
+	
 	local CHECK_DIR="$MDSC_SOURCE/$projectName/protected"
-	local BUILT_DIR="$MMDAPP/output/distro/$projectName/protected"
+	local BUILT_DIR="$MDSC_OUTPUT/$projectName/protected"
 	mkdir -p "$BUILT_DIR"
-	rsync -a -i --delete "$CHECK_DIR/" "$BUILT_DIR" 2>&1 | grep -v --fixed-strings --line-buffered '>f..t....... ' >&2
+	rsync -ai --delete "$CHECK_DIR/" "$BUILT_DIR" 2>&1 \
+	| (grep -v --line-buffered -E '>f\.\.t\.+ ' >&2 || true)
 }
 
-Require ListDistroProvides
-ListDistroProvides --select-changed --filter-and-cut "source-process" | grep -e " acm-domain$" | cut -d" " -f1 | sort | uniq | while read -r projectName ; do
-	Async "`basename "$projectName"`" MakeProjectAcmDomain "$projectName"
-	wait
-done
+MakeDistroAcmDomains(){
+	local projectName
+	Require ListDistroProvides
+	ListDistroProvides --select-changed --filter-and-cut "source-process" | grep -e " acm-domain$" | cut -d" " -f1 \
+	| sort | uniq \
+	| while read -r projectName ; do
+		Async "$( basename "$projectName" )" MakeProjectAcmDomain "$projectName"
+		wait
+	done
+}
+
+MakeDistroAcmDomains "$@"
