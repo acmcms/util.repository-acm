10ffd4e46e677b0fe1960bec356e4bc2da498384
Alexander I. Kharitchev <myx@myx.co.nz>
2021-02-02 12:58:48 +0200

    - actions: unneeded header

--- actions/acm/repository/sync-sources-git.sh
+++ actions/acm/repository/sync-sources-git.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [ -z "$MMDAPP" ] ; then
-	set -e
-	export MMDAPP="$( cd $(dirname "$0")/../../../../../.. ; pwd )"
-	echo "$0: Working in: $MMDAPP"  >&2
-	[ -d "$MMDAPP/source" ] || ( echo "expecting 'source' directory." >&2 && exit 1 )
-fi
+[ -d "$MMDAPP/source" ] || ( echo "ERROR: expecting 'source' directory." >&2 && exit 1 )
 
 . "$MMDAPP/source/acm/util.repository-acm/sh-scripts/sync-all-sources-acm.sh"
